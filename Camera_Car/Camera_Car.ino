09815ccbe34715f4c2f69af1caeb6ae10d0bc2d1
Jean-Louis Stanus <jlstanus@users.noreply.github.com>
2023-07-30 22:42:49 +0200

    Update Camera_Car.ino
    larger buffer https://github.com/espressif/esp-who/issues/30#issuecomment-455854533

--- Camera_Car/Camera_Car.ino
+++ Camera_Car/Camera_Car.ino
@@ -400,8 +400,8 @@ void setupCamera()
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
-  
-  config.frame_size = FRAMESIZE_VGA;
+
+  config.frame_size = FRAMESIZE_CIF;
   config.jpeg_quality = 10;
   config.fb_count = 1;
 
